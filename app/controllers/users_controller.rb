a09badb813cf7217968b406f0b5f0679325715b9
taro.kirameki <yokei.gomi@gmail.com>
2020-06-04 07:33:31 +0000

    timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id]=@user.id
       flash[:success]='ユーザーを登録しました' 
       redirect_to @user
     else
